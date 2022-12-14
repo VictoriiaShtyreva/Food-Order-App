32c1b13f4f9f27c3ad2913b18b99fd0ea14abcad
Viktoriia <vika1411akva0903@icloud.com>
2022-12-14 17:59:10 +0200

    feat: adding Portals for Modal and state for handler Cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 const App = () => {
+  // state manage of visible/not visible cart
+  const [cartIsShown, setCartIsShow] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  }
+
   return (
     <React.Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}/>
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
-}
+};
 
 export default App;
