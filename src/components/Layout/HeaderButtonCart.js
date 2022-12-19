5cd1f50420d81d3f7755dcc29d6a9d24d1d2c93d
kotkovsky <mariia.kotkova@exquance.com>
2022-12-19 18:53:34 +0200

    feat: add bump for HeaderButton

--- src/components/Layout/HeaderButtonCart.js
+++ src/components/Layout/HeaderButtonCart.js
@@ -1,19 +1,38 @@
-import React, {useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import CartIcon from "../Cart/CartIcon.js";
 import CartContext from "../../store/cart-context.js";
 import styles from "./HeaderButtonCart.module.css";
 
 const HeaderButtonCart = (props) => {
-const cartCtx = useContext(CartContext); //Now by using use context here, the header cart button component will be a re evaluated by react whenever the context changes.
+  const [buttonHighlited, setButtonHighlited] = useState(false);
+  const cartCtx = useContext(CartContext); //Now by using use context here, the header cart button component will be a re evaluated by react whenever the context changes.
 
-const numberOfCartItems = cartCtx.items.reduce((currentnumber, item) => {
-  return currentnumber + item.amount;
-}, 0);
+  const numberOfCartItems = cartCtx.items.reduce((currentnumber, item) => {
+    return currentnumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${styles.button} ${buttonHighlited ? styles.bump : ""}`;
+  const { items } = cartCtx; //for re-render only items
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonHighlited(true);
+    // set timer for remove class "bump"
+    const timer = setTimeout(() => {
+      setButtonHighlited(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer); //remove timer 
+    };
+  }, [items]);
 
   return (
-    <button className={styles.button} onClick={props.onClick}>
-      <span className={styles.icon}><CartIcon /></span>
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
       <span>Your cart</span>
       <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
