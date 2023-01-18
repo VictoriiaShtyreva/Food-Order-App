3767164abe0d34fefdcca4e330400d3ec0a8b0d5
kotkovsky <mariia.kotkova@exquance.com>
2023-01-18 14:17:45 +0200

    Fetching meals data

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,45 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealsItem from "./MealsItem/MealsItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Italian Chicken",
-    description:
-      "Grilled to perfection in Italian seasonings and low carb dressing, organic spinach, mixed greens, carrots, romaine, tomatoes, parmesan cheese & onions.",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Vegan Organic Grill Bowl",
-    description:
-      "Aorganic broccoli, cauliflower, snow peas, carrots, cabbage, green peppers, mushrooms & onions. Served on organic rice or (quinoa +.99)",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Organic Cheesecake",
-    description:
-      "One slice of our famous cheesecake topped with caramel sauce, turtle, mixed berry, or strawberry.",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Chicken Caesar Salad",
-    description:
-      "Organic Romaine heart chopped with parmesan cheese and low carb Caesar dressing.",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-19dbd-default-rtdb.firebaseio.com/meals.json"
+      );
+      const data = await response.json();
+
+      // Transform data for utilize
+      const loadedMeals = [];
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          // pull out of data for the given key
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+    
+  }, []);
+
   return (
     <section className={styles.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((meal) => (
+          {meals.map((meal) => (
             <MealsItem
               id={meal.id}
               key={meal.id}
